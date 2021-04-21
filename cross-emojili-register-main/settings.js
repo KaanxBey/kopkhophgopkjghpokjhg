134c6193c719dd348b0f73a189d0bf447f5044de
KaanxBey <76049819+KaanxBey@users.noreply.github.com>
2021-04-21 18:24:26 +0300

    Add files via upload

---
+++ cross-emojili-register-main/settings.js
@@ -0,0 +1,38 @@
+module.exports = {
+    bot: {
+        botOwner: ["719867771403632750"],
+        botToken: "ODMyNTc2NTg5NzQ4NTAyNTk4.YHlzXw.FD2pjgn5_dfdNHp2MEe5FCA7zbk",
+        botPrefix: ".",
+        botStatus: "Chewy ❤️ Wêsta"
+
+
+    },
+    guild: {
+        id: "832507946142662686",
+        tag: "Wêsta",
+        unTag: "•"
+    },
+
+    roles: {
+        manRoles: ["832516319210766367","832538001279287347","832538299008679966"],
+        womanRoles: ["832516965834686464","832538037732245514","832538243714646016"],
+        unregisterRoles: ["832516220309471232"],
+        registerStaff: "832511556503339058",
+        Suspecious: "832542335392284682",
+        vipRole: "833026841047466004",
+        boosterRole: "834424546747613184",
+        botCommands: "",
+        tagRole: "832544097302609921"
+    },
+    channel: {
+        rules: "832520737562427402",
+        botVoice: "832527570519457832",
+        registerChat: "832515582065770547"
+    },
+    emojis: {
+        yes: "834423624265891900",
+        no: "834423578203127859",
+        man: "834423725281640494", // erkek ve kız emoji ıd
+        woman: "834423806646157332"
+    }
+};
